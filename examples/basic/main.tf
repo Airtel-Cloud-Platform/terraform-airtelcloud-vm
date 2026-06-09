47d87651e18d382cbe17ee725c379f4ec25c51e8
airtelcloudplatform <acpterraformprovider@airtelworld.onmicrosoft.com>
2026-06-09 17:00:43 +0530

    Update main.tf

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,4 +1,5 @@
 module "vm" {
+
   source = "../../"
 
   vm_name = "web01"
@@ -6,9 +7,11 @@ module "vm" {
   os_type = "linux"
 
   flavor = "ccd.Large"
+  
   image  = "CentOS_Stream9_May2026"
 
   vpc_id    = "vpc-id"
+
   subnet_id = "subnet-id"
 
   availability_zone = "S1"
